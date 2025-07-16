1de5fdaf55a15974eb49281e8c18e6a6e75e7540
wakonig_k <klaus.wakonig@psi.ch>
2025-07-16 14:56:02 +0200

    fix: fix js mapping and cleanup

--- qtmonaco/qtmonaco/monaco_page.py
+++ qtmonaco/qtmonaco/monaco_page.py
@@ -7,4 +7,4 @@ logger = logging.getLogger(__name__)
 
 class MonacoPage(QWebEnginePage):
     def javaScriptConsoleMessage(self, level, message, line, source):
-        logger.debug(f"[JS Console] {level.name} at line {line} in {source}: {message}")
+        print(f"[JS Console] {level.name} at line {line} in {source}: {message}")
